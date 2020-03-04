44b45f9aeb5c3914314f1ef803197e15f9c9862d
Denis <den.kh@mail.ru>
2020-03-04 12:35:10 +0200

    footers added

---
+++ src/components/section1Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+function Section1Footer() {
+    return (
+        // <div className="footer1"><img
+        //     src="../images/redline.svg" alt="За стеклом"/>
+        //     <div className="footer_redline"></div>
+        <div className="footer1">
+            <div className="footer_block">
+                <div className="footer_redline">
+                    <div className="footer_text1">
+                        <p>NEVER MISTAKEN FOR ANYTHING ELSE</p>
+                    </div>
+                    <div className="footer_text2">
+                        <p>Candy canes topping dessert jelly beans pie jelly beans sweet roll. Toffee
+                            cotton candy
+                            cake chocolate cake. Bonbon gummi bears jelly-o bonbon fruitcake tootsie roll cake
+                            cookie gingerbread. Icing wafer pie pudding ice cream bonbon halvah bonbon topping.
+                            Donut halvah tiramisu bear claw tart.
+                        </p>
+                    </div>
+                    <div className="footer_text3">
+                        <p>Affordable prices Guaranteed cars</p>
+                        <button id="join_footer_button" className="footer_button">Join now!</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Section1Footer;
+
+
