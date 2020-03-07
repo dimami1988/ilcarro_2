3705d6acfbf385aafc218baf4903a9329ae96889
Denis <den.kh@mail.ru>
2020-03-07 13:14:35 +0200

    footers corrected

--- src/App.js
+++ src/App.js
@@ -6,66 +6,25 @@ import FeedBacks2Rows from "./components/FeedBacks2Rows";
 import SignUp from "./components/SignUp";
 //import Data from "./components/Data";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MostPopular from "./components/MostPopular";
 import LoginForm from "./components/LoginForm";
 import DatePicker1 from "react-datepicker/es";
 import SearchWithoutRegistrationBlock from "./components/SearchWithoutRegistration";
-import Section1Footer from "./components/section1Footer";
-import Section2Footer from "./components/section2Footer";
-import './App.css';
-import './Footers.css';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isShowSignUp: false
-        };
-
-    }
-    showSignUp= (isShowSignUp)=>{
-        this.setState({isShowSignUp:true})
-    };
-
-   render() {
-        if (this.state.isShowSignUp) {
-            return this.renderSignUp();
-        } else {
-            return this.renderMain();
-        }
-    }
-
-    renderMain() {
-        return (
-            <div className={'wrapper'}>
-                <MainHeader showSignUp={this.showSignUp}/>
-                <DatePicker1/>
-                <FindYourCarBlock showSignUp={this.showSignUp}/>
-                <Section1Footer/>
-                <MostPopular/>
-                <Section2MainBlock/>
-                <FeedBacks2Rows/>
-                <Section2Footer/>
-                <SearchWithoutRegistrationBlock/>
-            </div>
-
-        )
-    }
-
-
-  renderSignUp() {
+  render() {
     return (
         <div className={'wrapper'}>
-                  <MainHeader showSignUp={this.showSignUp}/>
+                  <MainHeader/>
             <DatePicker1/>
-            <SignUp/>
-            <Section1Footer/>
+            <FindYourCarBlock/>
             <MostPopular/>
             <Section2MainBlock/>
             <FeedBacks2Rows/>
-            <Section2Footer/>
             <LoginForm/>
+            <SignUp/>
             <SearchWithoutRegistrationBlock/>
         </div>
 
