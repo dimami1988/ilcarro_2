0107d5f40727eb32dcc72babb9f17da70679eff3
dimami1988 <dimami@ya.ru>
2020-03-20 00:38:11 +0200

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,132 +1,146 @@
 import React from "react";
 import MainHeader from "./components/MainHeader";
-import FindYourCarBlock from "./components/FindYourCarBlock";
 import Section2MainBlock from "./components/Section2MainBlock";
 import FeedBacks2Rows from "./components/FeedBacks2Rows";
-import SignUp from "./components/SignUp";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MostPopular from "./components/MostPopular";
-import LoginForm from "./components/LoginForm";
-import DatePicker1 from "react-datepicker/es";
-import SearchWithoutRegistrationBlock from "./components/SearchWithoutRegistration";
 import Section1Footer from "./components/section1Footer";
 import Section2Footer from "./components/section2Footer";
 import './App.css';
 import './Footers.css';
 import NavBar from "./components/NavBar";
-import RedButton from "./components/RedButton";
-import Map from "./components/Map";
-import Calendar from "./components/Calendar";
 import 'react-day-picker/lib/style.css';
 import RegStep1 from "./components/RegStep1";
 import MapContainer from "./components/Map";
+import Main from "./components/Main";
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            whichButtonPressed: "home"
-        };
-    }
-
-    showSignUp = (whichButtonPressed) => {
-        this.setState({whichButtonPressed: "registration"})
-    };
-
-    showLogin = (whichButtonPressed) => {
-        this.setState({whichButtonPressed: "login"})
-    };
-
-    showSearchWithoutRegistration = (whichButtonPressed) => (
-        this.setState({whichButtonPressed: "searchUnReg"})
-    );
-
-    showFindYourCar = (whichButtonPressed) => (
-        this.setState({whichButtonPressed: "home"})
-    );
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         whichButtonPressed: "home"
+    //     };
+    // }
+    //
+    // showSignUp = (whichButtonPressed) => {
+    //     this.setState({whichButtonPressed: "registration"})
+    // };
+    //
+    // showLogin = (whichButtonPressed) => {
+    //     this.setState({whichButtonPressed: "login"})
+    // };
+    //
+    // showSearchWithoutRegistration = (whichButtonPressed) => (
+    //     this.setState({whichButtonPressed: "searchUnReg"})
+    // );
+    //
+    // showFindYourCar = (whichButtonPressed) => (
+    //     this.setState({whichButtonPressed: "home"})
+    // );
 
     render() {
-        if (this.state.whichButtonPressed === "registration") {
-            return this.renderSignUp();
-        } else if (this.state.whichButtonPressed === "login") {
-            return this.renderLogin();
-        } else if (this.state.whichButtonPressed === "searchUnReg") {
-            return this.renderWithoutRegistration();
-        } else if (this.state.whichButtonPressed === "home") {
-            return this.renderFindYourCar();
-        }
-    }
-
-    renderFindYourCar() {
-        return (
+        return(
             <div>
-                <MainHeader showSignUp={this.showSignUp} showLogin={this.showLogin}
-                            showFindYourCar={this.showFindYourCar}
-                            showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <FindYourCarBlock showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <Section1Footer>
-                    <RedButton action={this.showSignUp}>Join Us</RedButton>
-                    {/*там где кнопка не нужна, я просто не пишу строку выше*/}
-                </Section1Footer>
-                {/*<Section1Footer showSignUp={this.showSignUp} whichButtonPressed={this.state.whichButtonPressed}/>*/}
+                <MainHeader/>
+                <Main/>
+                <Section1Footer/>
                 <MostPopular/>
                 <Section2MainBlock/>
                 <FeedBacks2Rows/>
-                <Section2Footer>
-                    <NavBar/>
-                </Section2Footer>
+                {/*<Section2Footer>*/}
+                {/*    <NavBar/>*/}
+                {/*<Section2Footer/>*/}
                 <RegStep1/>
                 <MapContainer/>
             </div>
-        )
-    }
-
-    renderLogin() {
-        return (
-            <div>
-                <MainHeader showSignUp={this.showSignUp} showFindYourCar={this.showFindYourCar}
-                            showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <LoginForm/>
-                <Section1Footer showSignUp={this.showLogin} showLogin={this.showLogin}
-                                whichButtonPressed={this.state.whichButtonPressed}/>
-                <MostPopular/>
-                <Section2MainBlock/>
-                <FeedBacks2Rows/>
-                <Section2Footer/>
-            </div>
+        // if (this.state.whichButtonPressed === "registration") {
+        //     return this.renderSignUp();
+        // } else if (this.state.whichButtonPressed === "login") {
+        //     return this.renderLogin();
+        // } else if (this.state.whichButtonPressed === "searchUnReg") {
+        //     return this.renderWithoutRegistration();
+        // } else if (this.state.whichButtonPressed === "home") {
+        //     return this.renderFindYourCar();
+        // }
         )
     }
 
 
-    renderSignUp() {
-        return (
-            <div>
-                <MainHeader showSignUp={this.showSignUp} showLogin={this.showLogin}
-                            showFindYourCar={this.showFindYourCar}
-                            showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <SignUp/>
-                <Section1Footer showSignUp={this.showSignUp} whichButtonPressed={this.state.whichButtonPressed}/>
-                <MostPopular/>
-                <Section2MainBlock/>
-                <FeedBacks2Rows/>
-                <Section2Footer/>
-            </div>
-        )
-    }
 
-    renderWithoutRegistration() {
-        return (
-            <div>
-                <MainHeader showSignUp={this.showSignUp} showFindYourCar={this.showFindYourCar}
-                            showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <SearchWithoutRegistrationBlock showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
-                <Section2Footer>
-                    <NavBar/>
-                </Section2Footer>
-            </div>
-        )
-    }
-}
+
+
+
+//
+//     renderFindYourCar() {
+//         return (
+//             <div>
+//                 <MainHeader showSignUp={this.showSignUp} showLogin={this.showLogin}
+//                             showFindYourCar={this.showFindYourCar}
+//                             showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <FindYourCarBlock showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <Section1Footer>
+//                     <RedButton action={this.showSignUp}>Join Us</RedButton>
+//                     {/*там где кнопка не нужна, я просто не пишу строку выше*/}
+//                 </Section1Footer>
+//                 {/*<Section1Footer showSignUp={this.showSignUp} whichButtonPressed={this.state.whichButtonPressed}/>*/}
+//                 <MostPopular/>
+//                 <Section2MainBlock/>
+//                 <FeedBacks2Rows/>
+//                 <Section2Footer>
+//                     <NavBar/>
+//                 </Section2Footer>
+//                 <RegStep1/>
+//                 <MapContainer/>
+//             </div>
+//         )
+//     }
+//
+//     renderLogin() {
+//         return (
+//             <div>
+//                 <MainHeader showSignUp={this.showSignUp} showFindYourCar={this.showFindYourCar}
+//                             showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <LoginForm/>
+//                 <Section1Footer showSignUp={this.showLogin} showLogin={this.showLogin}
+//                                 whichButtonPressed={this.state.whichButtonPressed}/>
+//                 <MostPopular/>
+//                 <Section2MainBlock/>
+//                 <FeedBacks2Rows/>
+//                 <Section2Footer/>
+//             </div>
+//         )
+//     }
+//
+//
+//     renderSignUp() {
+//         return (
+//             <div>
+//                 <MainHeader showSignUp={this.showSignUp} showLogin={this.showLogin}
+//                             showFindYourCar={this.showFindYourCar}
+//                             showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <SignUp/>
+//                 <Section1Footer showSignUp={this.showSignUp} whichButtonPressed={this.state.whichButtonPressed}/>
+//                 <MostPopular/>
+//                 <Section2MainBlock/>
+//                 <FeedBacks2Rows/>
+//                 <Section2Footer/>
+//             </div>
+//         )
+//     }
+//
+//     renderWithoutRegistration() {
+//         return (
+//             <div>
+//                 <MainHeader showSignUp={this.showSignUp} showFindYourCar={this.showFindYourCar}
+//                             showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <SearchWithoutRegistrationBlock showSearchWithoutRegistration={this.showSearchWithoutRegistration}/>
+//                 <Section2Footer>
+//                     <NavBar/>
+//                 </Section2Footer>
+//             </div>
+//         )
+//     }
+ }
 
 export default App;
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,16 +3,15 @@ import Logo from "../images/logo/LogoLight.svg";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
+import {Link} from "react-router-dom";
 
 class MainHeader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            whichButtonPressed: "home"
-
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         whichButtonPressed: "home"
+    //     };
+    // }
 
 
     render() {
@@ -21,29 +20,43 @@ class MainHeader extends React.Component {
                 <Container fluid={true}>
                     <Row>
                         <Col sm={5}>
-                            <div className='Logo' onClick={() => {
-                                this.props.showFindYourCar(this.state.whichButtonPressed)
-                            }}>
-                                <img src={Logo} alt={'logo'}  />
+                            <div className='Logo'>
+                                <Link to='/'>
+                                    <img src={Logo} alt={'logo'}/>
+                                </Link>
                             </div>
                         </Col>
 
                         <Col sm={7}>
                             <nav>
-                                <div className={"navBox"}><p className={'navigationText'} onClick={() => {
-                                    this.props.showSearchWithoutRegistration(this.state.whichButtonPressed)
-                                }}>Search</p></div>
-                                <div className={"navBox"}><p className={'navigationText'}>Let the car work</p></div>
-                                <div className={"navBox"}><a href={"https://en.wikipedia.org/wiki/Carsharing"}><p className={'navigationText'}>
-                                     Terms of use</p></a></div>
-                                <div className={"navBox"}><p onClick={() => {
-                                    this.props.showSignUp(this.state.whichButtonPressed)
-                                }}
-                                                             className={(this.props.whichButtonPressed === "login") ? 'hidden' : 'navigationText'}>Sign
-                                    up</p></div>
-                                <div className={"navBox"}><p onClick={() => {
-                                    this.props.showLogin(this.state.whichButtonPressed)
-                                }} className={'navigationText'}>Login</p></div>
+                                <div className={"navBox"}><p >
+                                    <Link to='/searchUnReg' className={'navigationText'}>
+                                        Search
+                                    </Link>
+                                </p></div>
+                                <div className={"navBox"}><p className={'navigationText'}>
+
+                                    Let the car work
+
+                                </p></div>
+                                <div className={"navBox"}><a href={"https://en.wikipedia.org/wiki/Carsharing"}><p
+                                    className={'navigationText'}>
+
+                                    Terms of use
+
+                                </p></a></div>
+                                <div className={"navBox"}><p>
+                                    <Link to='/signup' className={(this.props.whichButtonPressed === "login") ? 'hidden' : 'navigationText'}>
+                                        Sign up
+                                    </Link>
+                                </p>
+                                </div>
+
+                                <div className={"navBox"}><p>
+                                    <Link to='/login' className={'navigationText'}>
+                                        Login
+                                    </Link>
+                                </p></div>
                             </nav>
                         </Col>
 
